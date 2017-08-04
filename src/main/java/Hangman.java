14c28fa8eff55029e0ae92bba36f22a8fb3d0964
Christine Wasike <christinewasike22@gmail.com>
2017-08-04 10:45:03 +0300

    initial commit

---
+++ src/main/java/Hangman.java
@@ -0,0 +1,18 @@
+
+// import java.util.Random;
+
+import java.util.List;
+import java.util.Arrays;
+
+public class Hangman {
+    List<String> myWords = Arrays.asList("cow", "silver", "phone", "core");
+    List<String> usersGuess = Arrays.asList("");
+
+    public class Random {
+        Random myRandomGenerator = new Random();
+    }
+
+    public String selectWord(String returnsWordFromMyWords) {
+        return "hello";
+    }
+}
