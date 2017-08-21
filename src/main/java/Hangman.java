1d19ffc50181e052c594a7f27d34c7f44d8234b4
Christine Wasike <christinewasike22@gmail.com>
2017-08-21 09:52:21 +0300

    code uncommented and commented on.

--- src/main/java/Hangman.java
+++ src/main/java/Hangman.java
@@ -1,88 +1,86 @@
-
+import java.util.Scanner;
 import java.util.Random;
 
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class Hangman {
-    List<String> myWords = Arrays.asList("cow", "silver", "phone", "core");
-    Random myRandomGenerator = new Random();
+ public static void main(String[] args) {
+   Random random = new Random();
+   Scanner sc = new Scanner(System.in);
 
-    String word = myWords.get(myRandomGenerator.nextInt(myWords.size()));
+   //Create an Array to store my words that the user should guess
+   String[] words = {"moringa", "cohort", "salamander", "jarvis", "siri"};
+   boolean gamePlay = true;
 
-    public String findLetter(String word) {
+   while (gamePlay) {
+     System.out.println("Karibu Hangman");
+     //Pick a random word from the words array.
+     char[] randomWordArr = words[random.nextInt(words.length)].toCharArray();
+     int totalGuesses = randomWordArr.length;///Add more chances here later.
+     char[] playerGuess = new char[totalGuesses];
 
-        char[] filler = new char[word.length()];
-        int i = 0;
+     //convert all the elements in the array an underscore
+     for (int ges = 0; ges < playerGuess.length ; ges ++ ) {
+         playerGuess[ges] = '_';
+       }
+         boolean correctGuess = false;
+         int tries = 0;
 
-        while (i < word.length()) {
-            filler[i] = '-';
-            if (word.charAt(i) == ' ') {
-                filler[i] = ' ';
-            }
-            i++;
-        }
-        return word;
+         while(!correctGuess && tries != totalGuesses) {
+           System.out.print("Correct Guesses:");
+           //function to display the array of correct Guesses, dashes(_ _ _) if  empty
+           printArray(playerGuess);
+           //output to show tries left
+           System.out.printf("You have %d tries left.\n", totalGuesses - tries);
 
+           //prompt user to enter  a new character ,ONLY one character can be accepted
+           System.out.println("Enter a single Character");
+           char input =  sc.nextLine().charAt(0);
+           tries ++;
+           //if the player enters '-' the game ends
+           if (input == '-') {
+             //gamePlay = false;
+             correctGuess = true;
+           } else {
+               for (int ind = 0; ind < randomWordArr.length ; ind ++ ) {
+                 if(randomWordArr[ind] == input) {
+                 playerGuess[ind] = input;
+               }
+             }
+               if (isCorrectlyGuessed(playerGuess)) {
+               correctGuess = true;
+               System.out.println("Umefika Canaan!!!!");
+               printArray(playerGuess);
+
+             }
+           }
+
+
+         }
+         if(!correctGuess) System.out.println("You ran out of guesses");
+         System.out.println("Play again?(yes/no)");
+
+         String newGame = sc.nextLine();
+         if(newGame.equals("no")) gamePlay = false;
+
+
+
+   }
+   System.out.println("Game Over!");
+ }
+ //public method to print player guesses
+   public static void printArray(char[] array){
+     for (int x= 0; x < array.length ; x ++ ) {
+       System.out.print(array[x] + "  ");
+     }
+     System.out.println();
+   }
+
+   //public method to check if
+    public static boolean isCorrectlyGuessed(char[] array) {
+    for (int y =0; y< array.length  ; y ++ ) {
+      if (array[y] == '_') return false;
     }
+    return true;
 
-    Scanner scannedInput = new Scanner(System.in);
 
-    ArrayList<Character> usersGuess = new ArrayList<Character>();
-
-    char[] filler = new char[word.length()];
-
-    int i = 0;
-    int life = 5;
-    
-    while(i<word.length())
-    {
-        filler[i] = '-';
-        if (word.charAt(i) == ' ') {
-            filler[i] = ' ';
-        }
-        i++;
-    }System.out.print(filler);System.out.println("      life remaining = "+life);
-
-    Scanner s = new Scanner(System.in);
-
-    ArrayList<Character> l = new ArrayList<Character>();
-
-    while(life>0)
-    {
-        char x = s.next().charAt(0); //character input by user
-
-        if (l.contains(x + "")) {
-            System.out.println("Already entered");
-            continue; //while loop continues            
-        }
-
-        l.add(x);
-
-        if (word.contains(x + "")) {
-            for (int y = 0; y < word.length(); y++) {
-                if (word.charAt(y) == x) {
-                    filler[y] = x;
-                }
-
-            }
-        } else {
-            life--; //life decreases
-        }
-
-        if (word.equals(String.valueOf(filler))) { //checking if the filler equals word
-            System.out.println(filler);
-            System.out.println("You won mate  !!!");
-            break;
-        }
-
-        System.out.print(filler);
-        System.out.println("  life remaining = " + life);
-
-    }if(life==0)
-    {
-        System.out.println("You lose mate");
-    }
-}}
+}
+}
